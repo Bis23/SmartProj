98a0d96b9734c1c2198f4877e66cdda5216975aa
Bianca Santos <104869999+Bis23@users.noreply.github.com>
2024-04-21 23:42:34 -0300

    Update testPandas.py

--- Proj - Smart/testPandas.py
+++ Proj - Smart/testPandas.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
 
 def get_data(url):
     response = requests.get(url, verify=False)
@@ -10,6 +11,7 @@ api_url = "https://smartcampus-k8s.maua.br/api/timeseries/v0.1/smartcampusmaua/S
 data = get_data(api_url)
 
 df = pd.DataFrame(data)
-print(df)
+# print(df.describe())
 
 # colunas: fields, name, tags, timestamp
+print(df['fields'].apply(lambda x: x['data_humidity']))
