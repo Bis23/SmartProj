7ae6c4886c3e63da4f2eba7d9be259b25623a35a
Bianca Santos <104869999+Bis23@users.noreply.github.com>
2024-05-20 21:48:48 -0300

    Add files via upload

---
+++ smartproj-v3/testeConexao.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+
+def get_data(url):
+    response = requests.get(url, verify=False)
+    data = response.json()
+    return data
+
+api_url = "https://smartcampus-k8s.maua.br/api/timeseries/v0.1/smartcampusmaua/SmartLights/deviceId/0004a30b00e94a9d"
+data = get_data(api_url)
+
+df = pd.DataFrame(data)
+# print(df)
+
+
+# colunas: fields, name, tags, timestamp
+print(df['fields'].apply(lambda x: x['data_temperature']))
